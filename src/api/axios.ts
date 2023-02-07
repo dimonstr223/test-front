29cb54c8e6f4e52a59cde2e1274796c2cf12bc3b
dimonstr223 <dimstukmikolu@gmail.com>
2023-02-07 12:55:17 +0300

    baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'http://localhost:3001',
-	// baseURL: process.env.REACT_APP_API,
+	// baseURL: 'http://localhost:3001',
+	baseURL: process.env.REACT_APP_API,
 })
 
 export default instance
